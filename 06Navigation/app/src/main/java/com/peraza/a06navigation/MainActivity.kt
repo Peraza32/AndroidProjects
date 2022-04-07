44094e13fca4855a0bfa009dde542b060baaf556
Victor <00143320@uca.edu.sv>
2022-04-07 14:13:43 -0600

    Updates to names and SavedInstanceState

--- 06Navigation/app/src/main/java/com/peraza/a06navigation/MainActivity.kt
+++ 06Navigation/app/src/main/java/com/peraza/a06navigation/MainActivity.kt
@@ -2,12 +2,24 @@ package com.peraza.a06navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import com.peraza.a06navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: MainActivityBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.actionChangeFragment.setOnClickListener{
+            navController.navigate(R.id.action_homeFragment_to_secondScreenFragment)
+        }
+
     }
 }
